bac1d6827deeb1312131516d18ae9abb8d1e94db
Erhan Kılıç <erhan_kilic@outlook.com>
2017-09-05 18:42:20 +0300

    First problem solved.

---
+++ problems/problem1.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @Author: Erhan Kılıç
+ * @Website: http://erhankilic.org
+ * @Problem: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+ * The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+ * https://projecteuler.net/problem=1
+ */
+
+// Parameters
+$total = 0;
+
+for ($i = 1; $i < 1000; $i++) {
+    if ($i % 3 == 0 || $i % 5 == 0) {
+        $total += $i;
+    }
+}
+echo $total;
