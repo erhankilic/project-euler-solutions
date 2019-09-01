3660a14b67f6dea7190144cb8d37d485975256c0
Erhan Kılıç <erhan_kilic@outlook.com>
2019-09-01 19:56:16 +0300

    Problem 6 has been solved.

---
+++ php/problem6.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @Author: Erhan Kılıç
+ * @Website: http://erhankilic.org
+ * @Problem:
+The sum of the squares of the first ten natural numbers is,
+1^2 + 2^2 + ... + 10^2 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)^2 = 55^2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+
+ * https://projecteuler.net/problem=6
+ */
+
+$a = 385;
+$b = 55;
+
+for ($i = 11; $i <= 100; $i++) {
+    $a += $i**2;
+    $b += $i;
+}
+
+echo ($b**2) - $a;
